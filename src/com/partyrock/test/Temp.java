3eae3709310ef9b7587a8670a2294ffda2722763
Matthew <mvd7793@gmail.com>
2012-10-03 10:38:14 -0500

    Adding temp

---
+++ src/com/partyrock/test/Temp.java
@@ -0,0 +1,12 @@
+package com.partyrock.test;
+
+/**
+ * This is a quick file for testing whatever, including that the git repository
+ * is working correctly.
+ * @author Matthew
+ */
+public class Temp {
+	public static void main(String... args) {
+		System.out.println("Hello World!");
+	}
+}
