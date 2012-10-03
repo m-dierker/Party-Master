53106a239442a8894e41cd32c04cb472d71cdf4c
Matthew <mvd7793@gmail.com>
2012-10-03 11:08:09 -0500

    Finishing Temp

--- src/com/partyrock/test/Temp.java
+++ src/com/partyrock/test/Temp.java
@@ -2,11 +2,12 @@ package com.partyrock.test;
 
 /**
  * This is a quick file for testing whatever, including that the git repository
- * is working correctly.
+ * is working correctly. Feel free to test anything in this file, and commit
+ * whatever.
  * @author Matthew
  */
 public class Temp {
 	public static void main(String... args) {
-		System.out.println("Hello World! Party on!");
+		System.out.println("Party on!");
 	}
 }
