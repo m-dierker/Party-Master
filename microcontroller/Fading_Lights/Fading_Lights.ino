88e460670bea54b4e026c85cff20e22e52e73e30
Matthew <mvd7793@gmail.com>
2013-01-04 20:41:57 -0600

    Random stuff I've done that needs to be added

---
+++ microcontroller/Fading_Lights/Fading_Lights.ino
@@ -0,0 +1,24 @@
+void setup() {
+  pinMode(6, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+//  int pin = 6;
+//  for (int a = 0; a <= 255; a++) {
+//      analogWrite(pin, a);
+//      delay(5);
+//  }
+//  
+//  delay(1000);
+//
+//    for (int a = 255; a >= 0; a--) {
+//      analogWrite(pin, a);
+//      delay(5);
+//    }
+//  
+//  delay(1000);
+
+  analogWrite(6, 100);
+
+}
