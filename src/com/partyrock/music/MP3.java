84b6d096c20fd25bc36a95c21eccf37158613f4c
Matthew <mvd7793@gmail.com>
2013-01-05 22:59:25 -0600

    Fixed a warning in the MP3 class

--- src/com/partyrock/music/MP3.java
+++ src/com/partyrock/music/MP3.java
@@ -58,7 +58,6 @@ public class MP3 extends Sound {
 				AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, baseFormat.getSampleRate(), 16, baseFormat.getChannels(), baseFormat.getChannels() * 2, baseFormat.getSampleRate(), false);
 				din = AudioSystem.getAudioInputStream(decodedFormat, in);
 
-				DataLine.Info info = new DataLine.Info(SourceDataLine.class, decodedFormat);
 				line = AudioSystem.getSourceDataLine(decodedFormat, null);
 
 				if (line != null) {
