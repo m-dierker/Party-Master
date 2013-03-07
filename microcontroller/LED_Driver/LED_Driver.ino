32c8beaf2b2df9237b2fc21f97bb73f23232e08c
Brett Jackson <brettbj.jackson@gmail.com>
2013-03-07 02:23:55 -0600

    it's fully operational, kind of, but it still flickers problematically

--- microcontroller/LED_Driver/LED_Driver.ino
+++ microcontroller/LED_Driver/LED_Driver.ino
@@ -10,7 +10,8 @@ byte receivedBytes[2] = {0, 0};
 int byteOneTwo = 0;
 int ledLocation = 0;
 int aa = 0, bb = 0, cc = 0;
-int r = 0, g = 0, b = 0;
+byte r = 0, g = 0, b = 0;
+int counter = 0;
 
 // Clock and data pins are pins from the hardware SPI, you cannot choose them yourself if you use the hardware SPI.
 // Data pin is MOSI (Uno and earlier: 11, Leonardo: ICSP 4, Mega: 51, Teensy 2.0: 2, Teensy 2.0++: 22) 
@@ -68,24 +69,24 @@ void setup()
   
   
   
-  for (int a = 0; a < 8; a++)
-  {
-    for (int b = 0; b < 8; b++)
-    {
-      for (int c = 0; c < 3; c++)
-      {
-        if (a % 2 == 0 && b % 2 == 0 || a % 2 == 1 && b % 2 == 1) {
-          if (c == 2) {
-            panel[a][b][c] = 255;
-          }
-        } else {
-          if (c == 0){
-            panel[a][b][c] = 255;
-          }
-        }
-      }
-    }
-  }
+//  for (int a = 0; a < 8; a++)
+//  {
+//    for (int b = 0; b < 8; b++)
+//    {
+//      for (int c = 0; c < 3; c++)
+//      {
+//        if (a % 2 == 0 && b % 2 == 0 || a % 2 == 1 && b % 2 == 1) {
+//          if (c == 2) {
+//            panel[a][b][c] = 255;
+//          }
+//        } else {
+//          if (c == 0){
+//            panel[a][b][c] = 255;
+//          }
+//        }
+//      }
+//    }
+//  }
 }
 
 void loop()
@@ -163,33 +164,54 @@ void loop()
 
 void serialEvent() 
 { 
-  receivedBytes[byteOneTwo] = Serial.read();
-  byteOneTwo++;
-  
-  aa = floor(ledLocation / 24);
-  bb = floor(ledLocation / 8);
-  
-  if (byteOneTwo == 1)
+  if (byteOneTwo == 2)
   {
     byteOneTwo = 0;
+  }
+  receivedBytes[byteOneTwo] = Serial.read();
+
+  //Serial.println(counter);
+  //counter++;
+
+  
+  startTime = millis();
+  if (byteOneTwo == 1)
+  {
+    byteOneTwo = 2;
     processByte();
+    
+    aa = floor(ledLocation / 8);
+    bb = ledLocation % 8;
+    cc = 0;
+    
     panel[aa][bb][cc] = r;
     panel[aa][bb][cc+1] = g;
-    panel[aa][bb][cc+2] = b; 
+    panel[aa][bb][cc+2] = b;
+
+    ledLocation++;
   }
   
+  if (byteOneTwo == 0)
+  {
+    byteOneTwo = 1;
+  }
+  durTime = millis() - startTime;
+  Serial.println(durTime);
    
 }
 
 void processByte()
 {
-  if (receivedBytes[0] == B10110111 && receivedBytes[1] == B01111000)
+  if (receivedBytes[0] == B10110111 & receivedBytes[1] == B01111000)
+  {
+    ledLocation = 0;
+  }
+  if (ledLocation >= 64*3-1)
   {
     ledLocation = 0;
   }
   
   r = receivedBytes[0] & B01111100;
   g = (receivedBytes[0] & B00000011) | (receivedBytes[1] & B11100000);
-  b = receivedBytes[1] & B00011111; 
-  ledLocation++;
+  b = receivedBytes[1] & B00011111;
 }
