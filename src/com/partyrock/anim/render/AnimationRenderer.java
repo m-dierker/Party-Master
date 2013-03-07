a2313843280710cca467a869c896e9854c3753f7
Matthew <mvd7793@gmail.com>
2013-03-06 22:06:18 -0600

    Adding more stuff

---
+++ src/com/partyrock/anim/render/AnimationRenderer.java
@@ -0,0 +1,29 @@
+package com.partyrock.anim.render;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
+
+import com.partyrock.anim.Animation;
+
+/**
+ * Renders an animation
+ * 
+ * @author Matthew
+ * 
+ */
+public class AnimationRenderer {
+    private Animation animation;
+
+    public AnimationRenderer(Animation animation) {
+        this.animation = animation;
+    }
+
+    public Animation getAnimation() {
+        return animation;
+    }
+
+    public void renderAnimation(GC gc, Rectangle cellBounds) {
+        gc.setBackground(animation.sysColor(SWT.COLOR_BLUE));
+    }
+}
